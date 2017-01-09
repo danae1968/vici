3d2f8ce2ff7ff31d3e16b21f3ab3e84187bbf688
Danae Papadopetraki <danaepapadopetraki@gmail.com>
2017-01-09 07:49:45 +0100

    pilot 5 results

--- QuantifyingCC/Log/ChoiceTask/choicesforRNoRedo.m
+++ QuantifyingCC/Log/ChoiceTask/choicesforRNoRedo.m
@@ -1,7 +1,7 @@
 function [IPggplot,choicesR,keyAnal]=choicesforRNoRedo
 
 cd(pwd);
-numSubs=1:32; numSubs(4:7)=[];
+numSubs=1:7; numSubs(6)=[];
 fixedValue=2;
 easyOffer=[0.1 round((0.2:0.2:2.2)*10)/10];
 [IPmatrix,normSV]=regressionChoicesNoRedo(numSubs,fixedValue,easyOffer);
@@ -11,9 +11,11 @@ choicesR=[];choicesRNA=[];keyResp=[];
 %2:4 if easy offer/Direct is sz1 1:4 if No redo
 setSize=1:4;
 condition=[0 2];
+sesNo=1:3;
 for i=numSubs
+    for j=sesNo
     
-    participant=sprintf('ColorFunChoice_s%d.mat',i);
+    participant=sprintf('ColorFunChoice_s%d_ses%d.mat',i,j);
     load(participant)
     
     for n=1:length(data.typeTask)
@@ -32,25 +34,26 @@ for i=numSubs
             if data.choice(n)~=9
                 choicesR=[choicesR;i data.condition(n) data.sz(n) data.easyOffer(n) data.choice(n) data.choiceRT(n) data.block(n)];
             elseif data.choice(n)==9
-                choicesRNA=[choicesRNA;i data.condition(n) data.sz(n) data.easyOffer(n) data.choice(n) data.choiceRT(n) data.block(n)];
+                choicesRNA=[choicesRNA;i j data.condition(n) data.sz(n) data.easyOffer(n) data.choice(n) data.choiceRT(n) data.block(n)];
             end
         end
     end
-end
-%  csvwrite('choicesRprepilot2',choicesR)
-%% check if they responded with same key
-keyFirst=keyResp(:,1:2); %first response
-subNo=keyResp(:,1);
-keyAnal=zeros(length(numSubs),2);
-for j=numSubs
-    keyAnal(j,:)=histc(keyFirst(subNo==j,2),[49, 50])';
-end
-reSV=reshape(normSV,size(normSV,1)*size(normSV,2),1);
-reIP=reshape(IPmatrix,size(IPmatrix,1)*size(IPmatrix,2),1);
-part=repmat([numSubs]',length(reSV)/length(numSubs),1);part=part(:);
-SZ=repmat(setSize,length(reSV)/(2*length(setSize)),2);SZ=SZ(:);
-% SZ=repmat(setSize,numSubs,1);SZ=SZ(:);
-cond=repmat(condition,length(reSV)/2,1);cond=cond(:);
-
-IPggplot=[part reSV reIP SZ cond];
+    end
+ end
+% %  csvwrite('choicesRprepilot2',choicesR)
+% %% check if they responded with same key
+% keyFirst=keyResp(:,1:2); %first response
+% subNo=keyResp(:,1);
+% keyAnal=zeros(length(numSubs),2);
+% for j=numSubs
+%     keyAnal(j,:)=histc(keyFirst(subNo==j,2),[49, 50])';
+% end
+% reSV=reshape(normSV,size(normSV,1)*size(normSV,2),1);
+% reIP=reshape(IPmatrix,size(IPmatrix,1)*size(IPmatrix,2),1);
+% part=repmat([numSubs]',length(reSV)/length(numSubs),1);part=part(:);
+% SZ=repmat(setSize,length(reSV)/(2*length(setSize)),2);SZ=SZ(:);
+% % SZ=repmat(setSize,numSubs,1);SZ=SZ(:);
+% cond=repmat(condition,length(reSV)/2,1);cond=cond(:);
+% 
+% IPggplot=[part reSV reIP SZ cond];
 %   csvwrite('IPGGplotNoRedo',IPggplot)
--- QuantifyingCC/Log/Colorwheel/responses3sessions.m
+++ QuantifyingCC/Log/Colorwheel/responses3sessions.m
@@ -62,5 +62,8 @@ end
 
 statmatAcc=[I1s1' I1s2' I1s3' I2s1' I2s2' I2s3' I3s1' I3s2' I3s3' I4s1' I4s2' I4s3' U1s1' U1s2' U1s3' U2s1' U2s2' U2s3' U3s1' U3s2' U3s3' U4s1' U4s2' U4s3'];
 statmatAcc(6,:)=[];
+statmatRT=[I1s1RT' I1s2RT' I1s3RT' I2s1RT' I2s2RT' I2s3RT' I3s1RT' I3s2RT' I3s3RT' I4s1RT' I4s2RT' I4s3RT' U1s1RT' U1s2RT' U1s3RT' U2s1RT' U2s2RT' U2s3RT' U3s1RT' U3s2RT' U3s3RT' U4s1RT' U4s2RT' U4s3RT'];
+statmatRT(6,:)=[];
 
-xlswrite('MedianAcc.xlsx',statmatAcc)
+% xlswrite('MedianAcc.xlsx',statmatAcc)
+xlswrite('MedianRT.xlsx',statmatRT)
